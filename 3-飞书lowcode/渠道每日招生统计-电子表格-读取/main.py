bc62fde6cdb9e2680070479b54c321d69bbdcf6e
xujunhao2 <xujunhao2@hetao101.com>
2025-01-21 11:04:15 +0800

    增加写入mysql

--- 3-飞书lowcode/渠道每日招生统计-电子表格-读取/main.py
+++ 3-飞书lowcode/渠道每日招生统计-电子表格-读取/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import datetime as dt
 
 #os.environ['myconf'] = "/Users/xujunhao/PycharmProjects/Learn-Python/my_packages" #设置一个自定义模块路径，并通过环境变量存储路径
 sys.path.append(os.getenv('myconf')) #动态将该路径添加到 Python 的模块搜索路径中
@@ -10,7 +11,11 @@ from fs_spreadsheet import *
 conf = yaml.full_load(open('conf.yml', 'r', encoding='utf-8'))
 app_id = conf['feishu_app']['app_id']
 app_secret = conf['feishu_app']['app_secret']
-webhook_url=conf['feishu_robot']['webhook']
+file = conf['file']
+table = conf['table']
+comment = conf['comment']
+col_comment = conf['col_comment']
+table_index = conf['table_index']
 
 # 配置参数
 app_token = "CkDBsZzdYhXtPJtOTxDcR6yynSd"
@@ -21,6 +26,23 @@ tenant_access_token = get_tenant_access_token(app_id,app_secret)
 
 range="A1:B10"
 
-data=get_data(tenant_access_token,app_token,table_id,range)
+df=get_data(tenant_access_token,app_token,table_id,range)
 
-#print(data)
+# 创建data文件夹
+data_path = file.split('/')[0]
+if not os.path.exists(data_path):
+    os.mkdir(data_path)
+
+#写入到feather文件
+insert_db.save_to_feather(df,file)
+
+# 连接mysql
+mysql_conn, mysql_cursor = insert_db.connect_to_mysql()
+
+# 创建表
+insert_db.create_or_truncate_mysql_table(mysql_cursor, table, col_comment)
+
+# 导入数据
+print('import_to_mysql_start_time：', dt.datetime.now())
+insert_db.import_to_mysql(mysql_conn, mysql_cursor, table, file)
+print('import_to_mysql_end_time：', dt.datetime.now())
